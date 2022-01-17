db944346e666bea74db1aa7cb54dffa34dd0b2ad
ViniciusRam <viniciuseduardorr@gmail.com>
2022-01-17 18:06:01 -0300

    criação do header mobile

---
+++ js/index.js
@@ -0,0 +1,53 @@
+(function sandwichMenu(){
+
+
+    function toggleMenu(event) {
+        console.log('entrou')
+
+        if (event.type === 'touchstart') {event.preventDefault()}
+
+        arr.push('.')
+
+        const navContainer = document.getElementById('navMenu');
+        const _class = navContainer.classList
+
+        const bodyContainer = document.getElementById('body');
+        const _body = bodyContainer.classList
+
+        if(( arr.length % 2) !== 0 ){
+            _class.add('active')
+            _body.add('active')
+        } else {
+            _class.remove('active')
+            _body.remove('active')
+        }
+        const active = navContainer.classList.contains('active')
+        event.currentTarget.setAttribute('aria-expanded', active)
+
+        active ? event.currentTarget.setAttribute('aria-label', 'fechar menu') : event.currentTarget.setAttribute('aria-label', 'abrir menu')
+        
+        animateLinks()
+
+        if(_body.contains('active')){
+            bodyContainer.style.overflow = 'hidden'
+        } else {
+            bodyContainer.style.overflow = 'visible'
+        }
+    }
+
+    function animateLinks(){    
+            links.forEach((link, index) => {
+                link.style.animation ? (link.style.animation = ``) : (link.style.animation = `aFade .3s ease forwards ${index / 7 + 0.001}s`);
+            })
+    }
+
+    const btnMobileMenu = document.getElementById('sandwichMenu')
+    const links = document.querySelectorAll('.navMenu')
+
+    const arr = []
+
+
+    btnMobileMenu.addEventListener('click', toggleMenu)
+    btnMobileMenu.addEventListener('touchstart', toggleMenu)
+
+})();
