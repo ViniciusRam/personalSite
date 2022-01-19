b79ff45f0967894fbe020b7a5280e8ab4f80018e
ViniciusRam <viniciuseduardorr@gmail.com>
2022-01-18 21:37:15 -0300

    adicionando animação nos links do menu

--- js/index.js
+++ js/index.js
@@ -1,43 +1,42 @@
 (function sandwichMenu(){
 
+    const btnMobileMenu = document.getElementById('sandwichBtn');
+    const links = document.querySelectorAll('.navLinks a');
+    const arr = [];
+    btnMobileMenu.addEventListener('click', toggleMenu);
+    btnMobileMenu.addEventListener('touchstart', toggleMenu);
 
-    function toggleMenu(event) {
-        console.log('entrou')
-
-        if (event.type === 'touchstart') {event.preventDefault()}
-
-        arr.push('.')
-
-        const navContainer = document.getElementById('navMenu');
-        const _class = navContainer.classList
-
-
-        if(( arr.length % 2) !== 0 ){
-            _class.add('active')
-        } else {
-            _class.remove('active')
-        }
-        const active = navContainer.classList.contains('active')
-        event.currentTarget.setAttribute('aria-expanded', active)
-
-        active ? event.currentTarget.setAttribute('aria-label', 'fechar menu') : event.currentTarget.setAttribute('aria-label', 'abrir menu')
-        
-        animateLinks()
-    }
-
-    function animateLinks(){    
+    function animateLinks(_class){    
             links.forEach((link, index) => {
-                link.style.animation ? (link.style.animation = ``) : (link.style.animation = `aFade .3s ease forwards ${index / 7 + 0.001}s`);
+                if (_class.contains('active')){
+                    link.style.animation = `menuLinksFade .3s ease forwards ${index / 7 + 0.001}s`;
+                } else {
+                    link.style.animation = `menuLinksFadeRev .3s ease forwards`;
+                }
             })
     }
 
-    const btnMobileMenu = document.getElementById('sandwichBtn')
-    const links = document.querySelectorAll('.navMenu')
-
-    const arr = []
+    function toggleMenu(event) {
 
 
-    btnMobileMenu.addEventListener('click', toggleMenu)
-    btnMobileMenu.addEventListener('touchstart', toggleMenu)
+        if (event.type === 'touchstart') {event.preventDefault()};
 
+        arr.push('.');
+
+        const navContainer = document.getElementById('navMenu');
+        const _class = navContainer.classList;
+
+
+        if(( arr.length % 2) !== 0 ){
+            _class.add('active');
+        } else {
+            _class.remove('active');
+        }
+        const active = navContainer.classList.contains('active');
+        event.currentTarget.setAttribute('aria-expanded', active);
+
+        active ? event.currentTarget.setAttribute('aria-label', 'fechar menu') : event.currentTarget.setAttribute('aria-label', 'abrir menu');
+        
+        animateLinks(_class);
+    }
 })();
